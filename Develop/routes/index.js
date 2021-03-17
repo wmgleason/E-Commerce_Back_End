e3b4e21c5210a315831066a97fd1ee35c78d425e
Wanda Gleason <oglissane@gmail.com>
2021-03-16 23:15:58 -0400

    Added comment re the api.routes line.

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+//THIS TELLS THE WHOLE APP THAT THE ROUTES WILL BE FOUND IN /api folder so it isn't shown explicitly elsewhere
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
