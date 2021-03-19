5005ab7e7871d7b64352d3b5351767281050cad4
Wanda Gleason <oglissane@gmail.com>
2021-03-19 19:00:42 -0400

    Corrected typo

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,30 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      // This is a reference to another model
+      model: 'product',
+      // This is the column name of the referenced model
+      key: 'id',
+      }
+  },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This is a reference to another model
+        model: 'tag',
+        // This is the column name of the referenced model
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
