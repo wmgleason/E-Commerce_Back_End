7799a7f2283527a61b30e2e21f966b0720fba8bf
Wanda Gleason <oglissane@gmail.com>
2021-03-20 19:27:53 -0400

    Deleted a comment.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -50,7 +50,3 @@ module.exports = {
 };
 
 
-Product belongs to Category, and Category has many Product models, as a category can have multiple products but a product can only belong to one category.
-
-Product belongs to many Tag models, and Tag belongs to many Product models. Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
-
