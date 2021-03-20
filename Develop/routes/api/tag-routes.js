9602a9136bc021630e81baceb9fda216d1dd2038
Wanda Gleason <oglissane@gmail.com>
2021-03-20 19:22:04 -0400

    Added routes for GET POST UPDATE

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,99 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: ProductTag }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: ProductTag }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// router.put('/:id', (req, res) => {
+//   // update a tag's name by its `id` value
+//   try {
+//     const tagData = await Tag.update(req.body {
+//       where: {
+//         id: req.params.id,
+//       },
+//       individualHooks: true
+//     });
+//     if (!tagData[0]) {
+//       res.status(404).json({ message: 'Update failed - no tag found with that id!' });
+//       return;
+//     }
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+// This action method is the Controller. It accepts input and sends data to the Model and the View.
+router.put('/:id', async (req, res) => {
+  // It is sending the data to the Model so that one category can be updated with new data in the database.
+  try {
+    const tag = await Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+    // If the database is updated successfully, what happens to the updated data below?
+    // The updated data is then sent back to handler that dispatched the request.
+    res.status(200).json(tag);
+  } catch (err) {
+      res.status(500).json(err);
+    };
 });
+
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete one tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
