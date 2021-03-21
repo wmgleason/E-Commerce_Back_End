41393c1e6947d7bea67739cdba6feade55b81cf5
Wanda Gleason <oglissane@gmail.com>
2021-03-21 17:19:17 -0400

    Working on GET error.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -8,8 +12,20 @@ require('dotenv').config();
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    });
+      port: 3301
+    }
+  );
+}
+
+  // sequelize = new Sequelize(
+  //   process.env.DB_NAME,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASSWORD,
+  //   {
+  //     host: 'localhost',
+  //     dialect: 'mysql',
+  //     port: 3306
+  //   });
 
 // const sequelize = process.env.JAWSDB_URL
 //   ? new Sequelize(process.env.JAWSDB_URL)
