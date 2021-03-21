12f663e493dc37abd5fee00ac0a29bd58462d1f4
Wanda Gleason <oglissane@gmail.com>
2021-03-21 15:22:08 -0400

    Deleted stray curly bracket

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -9,9 +9,8 @@ require('dotenv').config();
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
-  );
-}
+    });
+
 // const sequelize = process.env.JAWSDB_URL
 //   ? new Sequelize(process.env.JAWSDB_URL)
 //   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
