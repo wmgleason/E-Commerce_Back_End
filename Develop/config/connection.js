ecdf4e5b1c06780751379c54f0f36691e6b5c1dd
Wanda Gleason <oglissane@gmail.com>
2021-03-20 23:03:22 -0400

    Cleaning up the code

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,6 @@
-require('dotenv').config();
-const mysql = require('mysql');
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
