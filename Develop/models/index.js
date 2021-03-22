850747c2238fd72607f0ccc6e952df0b1ca49149
Wanda Gleason <oglissane@gmail.com>
2021-03-21 21:48:06 -0400

    Changing model names to be consistent throughout.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,7 +11,7 @@ Product.belongsTo(Category, {
 // Category has many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  // onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
