cc8d9749116c24f3598d55a3a4eb900bb1efbcb3
Wanda Gleason <oglissane@gmail.com>
2021-03-21 22:39:16 -0400

    Typo fixed.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const Sequelize = require('sequelize');
 
 // let sequelize;
@@ -23,7 +23,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3301,
+      port: 3001,
       dialectOptions: {
         decimalNumbers: true,
       },
