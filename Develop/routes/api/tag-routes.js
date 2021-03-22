0ad869b9867b85097a6ac70328c0f3fe658c7b74
Wanda Gleason <oglissane@gmail.com>
2021-03-21 22:49:40 -0400

    Removed asyncs.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,45 +3,63 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findAll({
-      include: [{ model: ProductTag }],
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: ProductTag }],
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with this id'});
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
-      return;
-    }
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   // create a new tag
-  try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(400).json({ message: 'Did not create a new tag!' });
-  }
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
+
 
 // router.put('/:id', (req, res) => {
 //   // update a tag's name by its `id` value
@@ -62,26 +80,30 @@ router.post('/', async (req, res) => {
 //   }
 // });
 // This action method is the Controller. It accepts input and sends data to the Model and the View.
-router.put('/:id', async (req, res) => {
   // It is sending the data to the Model so that one category can be updated with new data in the database.
-  try {
-    const tag = await Tag.update(
-    {
-      tag_name: req.body.tag_name
-    },
-    {
+  router.put('/:id', (req, res) => {
+    // update a tag's name by its `id` value
+    Tag.update(req.body, {
       where: {
-        id: req.params.id,
-      },
-    });
+          id: req.params.id
+      }
+    })
+      .then(dbTagData => {
+          if (!dbTagData[0]) {
+              res.status(404).json({ message: 'No tag found with this id'});
+              return;
+          }
+          res.json(dbTagData);
+    })
     // If the database is updated successfully, what happens to the updated data below?
     // The updated data is then sent back to handler that dispatched the request.
-    res.status(200).json(tag);
-  } catch (err) {
-      res.status(500).json(err);
-    };
-});
-
+      .catch(err => {
+          console.log(err); 
+          res.status(500).json(err);
+    });
+  
+  });
+    
 
 router.delete('/:id', async (req, res) => {
   // delete one tag by its `id` value
