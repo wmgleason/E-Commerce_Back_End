4f02ef1c23d0a1b20e70139869a36ebb98827149
Wanda Gleason <oglissane@gmail.com>
2021-03-21 22:02:04 -0400

    Corrected port number.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -23,7 +23,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
+      port: 3301,
       dialectOptions: {
         decimalNumbers: true,
       },
