5ee19159f86017913a27fbd43b227152bf068a24
Wanda Gleason <oglissane@gmail.com>
2021-03-21 21:51:27 -0400

    Edited out async due to error.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({ message: 'Did not create a new tag!' });
   }
 });
 
